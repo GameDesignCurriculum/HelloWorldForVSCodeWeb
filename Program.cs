39a02c3fdb90b6255c6f3e4cafd871049a3f515b
Ms. Gallant <patricia.gallant@neric.org>
2024-01-25 11:01:58 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using system;
+
+namespace HelloWorld
+    {
+    internal class Program
+        {
+        static void Main(string[] args)
+            {
+            Console.WriteLine("Hello, World!");
+            }
+        }
+    }
